f537a2cb2fd564b30d629b12bd3c61e6ac55248e
Alan Jassan <alanjassan@hotmail.com>
2022-03-31 18:53:14 -0300

    Revert "Revert "second commit""
    This reverts commit b8c0618b25cc33d23e0fdbe35628c61241257d1c.

---
+++ tp2_1_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    #define N 20
+
+    int i;
+    int vt[N];
+
+    for(i = 0;i<N; i++)
+    {
+    vt[i]=1+rand()%100;
+    printf("%i", vt[i]);
+    }
+    return 0;
+}
+
